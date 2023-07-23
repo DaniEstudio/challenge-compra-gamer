df48c805ac7a1bbf8b747314d8df589065cd79fb
DaniEstudio <daniela.gavilan.estudiante@gmail.com>
2023-07-23 13:56:29 -0300

    feature/ 015 - Borrar componente Grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { GridComponent } from './grid/grid.component';
+import { ProductListComponent } from './product-list/component/product-list.component';
+import { CartListComponent } from './cart/component/cart-list.component';
+import { LandingComponent } from './landing/landing.component';
 
 const routes: Routes = [
-  { path:'', component: GridComponent },
+  { path:'', component: LandingComponent },
+  { path:'product', component: ProductListComponent },
+  { path:'cart', component: CartListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,17 @@ import { CardComponent } from './shared/card/component/card.component';
 import { AlphabeticalOrderPipe } from './shared/pipe/alphabetical-order.pipe';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SearchBarComponent } from './shared/search-bar/component/search-bar.component';
-import { GridComponent } from './grid/grid.component';
 import { ProductSearchService } from './shared/service/product-search.service';
+import { CartListComponent } from './cart/component/cart-list.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    GridComponent,
+    LandingComponent,
     ProductListComponent,
+    CartListComponent,
     CardComponent,
     AlphabeticalOrderPipe,
     SearchBarComponent
