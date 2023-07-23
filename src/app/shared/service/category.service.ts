fa5eacb33f64fe4c2e85f03ca34322fe4ee6e001
DaniEstudio <daniela.gavilan.estudiante@gmail.com>
2023-07-23 20:46:52 -0300

    rfeature 0212- SAgregar Hero seccion con imagen para categorias

--- src/app/shared/service/category.service.ts
+++ src/app/shared/service/category.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { CategoryModel } from '../../navbar/model/category.model';
 
 @Injectable({
@@ -9,11 +9,22 @@ import { CategoryModel } from '../../navbar/model/category.model';
 export class CategoryService {
 
   private API_URL = 'https://static.compragamer.com/test/subcategorias.json';
+  private IMG_BASE_URL: string = 'https://compragamer.net/categorias_demo/';
+  private categoryImgURLSubject: Subject<string> = new Subject<string>();
 
   constructor( private http: HttpClient ) { }
 
   getCategoryData(): Observable<CategoryModel[]> {
     return this.http.get<CategoryModel[]>(this.API_URL);
   }
+
+  getCategoryImgURL(): Observable<string> {
+    return this.categoryImgURLSubject.asObservable();
+  }
+
+  setCategoryImgURL(imgName: string | null): void {
+    this.categoryImgURLSubject.next(imgName !== null ? this.IMG_BASE_URL + imgName : null);
+  }
+
 
 }
