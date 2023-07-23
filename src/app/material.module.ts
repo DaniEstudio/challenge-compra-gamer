eecccce2542f3718f965399d99fe31d836527fd6
DaniEstudio <daniela.gavilan.estudiante@gmail.com>
2023-07-23 13:57:44 -0300

    feature/ 016 - Modificar estilos

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
   imports: [
     MatToolbarModule,
     MatExpansionModule,
     MatButtonModule,
-    MatCardModule
+    MatButtonToggleModule,
+    MatCardModule,
+    MatIconModule,
+    MatBadgeModule
   ],
   exports: [
     MatToolbarModule,
     MatExpansionModule,
     MatButtonModule,
+    MatButtonToggleModule,
     MatCardModule,
+    MatIconModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule {}
